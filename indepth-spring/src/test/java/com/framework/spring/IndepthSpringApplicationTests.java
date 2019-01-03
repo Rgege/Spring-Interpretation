5158a50f8df7c3af58a1c46ec77c3382afdc7d82
m15801983447@168.com <xrln47928>
2019-01-03 22:13:36 +0800

    test commit code by idea

--- indepth-spring/src/test/java/com/framework/spring/IndepthSpringApplicationTests.java
+++ indepth-spring/src/test/java/com/framework/spring/IndepthSpringApplicationTests.java
@@ -1,16 +1,28 @@
 package com.framework.spring;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+/**
+ * base test class
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class IndepthSpringApplicationTests {
+    @Before
+    public void init() {
+        System.out.println("开始测试-----------------");
+    }
 
-    @Test
-    public void contextLoads() {
+    @After
+    public void after() {
+        System.out.println("测试结束-----------------");
     }
 
 }
