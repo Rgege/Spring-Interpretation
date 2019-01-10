6b7de4b423d8e368bdd1880732a225c681450369
m15801983447@163.com <xrln47928>
2019-01-10 18:17:14 +0800

    modify 4 import

--- spring-framework-master/explore-spring/src/main/java/com/explore/sourceCodeTest/beansSC/pojo/AllenPojo.java
+++ spring-framework-master/explore-spring/src/main/java/com/explore/sourceCodeTest/beansSC/pojo/AllenPojo.java
@@ -1,11 +1,13 @@
 package com.explore.sourceCodeTest.beansSC.pojo;
 
+
 /**
- * @Description <P></P>
- * @Author m15801983447@163.com
- * @Date 2019/1/9
- * @Version 1.0.0
- **/
+ * pojo for test.
+ *
+ * @author Rossen Stoyanchev
+ * @author Rob Winch
+ * @since 1.0
+ */
 public class AllenPojo {
-	
+
 }
