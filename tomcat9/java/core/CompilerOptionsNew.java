fd8e419bbdbc17f760788a212b0f3acb0ee5973d
m15801983447@163.com <xrln47928>
2019-06-25 21:18:48 +0800

    delete tomcat9 source code

--- tomcat9/java/core/CompilerOptionsNew.java
+++
@@ -1,8 +0,0 @@
-package core;
-
-import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
-
-public class CompilerOptionsNew extends CompilerOptions {
-    public static final String VERSION_10="10.0";
-    public static final String VERSION_9 = "1.9";
-}
